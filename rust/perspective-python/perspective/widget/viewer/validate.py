e0b75ce5fca350f8c3dcc2e42ae092702c642cb9
Tom Jakubowski <tom@prospective.dev>
2025-05-15 11:27:41 -0700

    fix version traitlet bug

--- rust/perspective-python/perspective/widget/viewer/validate.py
+++ rust/perspective-python/perspective/widget/viewer/validate.py
@@ -10,13 +10,18 @@
 #  ┃ of the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0). ┃
 #  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
+from traitlets import TraitError
+
 
 def validate_version(version):
     # basic semver of form \d+\.\d+\.\d+(\+.+)?
     spl = version.split(".", 2)
-    return (
+    if (
         len(spl) == 3
         and spl[0].isdigit()
         and spl[1].isdigit()
         and (spl[2].split("+")[0]).isdigit()
-    )
+    ):
+        return version
+    else:
+        raise TraitError(f"The 'version' must be in the form X.Y.Z, not {version}")
